10285eb9e26ce6727ff8481e2c21feb697f6b551
tiedong <2547337875@qq.com>
2018-04-17 14:21:48 +0800

    提交pdo的lastinstertId对象

---
+++ 01-pdoLastInstrentId.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018-04-17
+ * Time: 14:05
+ */
+
+//实例化pdo对象
+$dsn = "mysql:host=106.75.129.183;dbname=test;port=3306;charset=utf8";
+$user = 'root';
+$pass = 'myroot123';
+$pdo = new PDO($dsn,$user,$pass);
+
+//插曲一条语句
+$sql = "INSERT INTO user VALUES (NULL,'WangWu','123456')";
+$pdo->exec($sql);
+$id = $pdo->lastInsertId();
+var_dump($id);
