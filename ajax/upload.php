f5664bcb2ea64dba2a5c213b70a8d42871727c3e
tiedong <2547337875@qq.com>
2018-10-10 18:12:29 +0800

    dsvs

--- ajax/upload.php
+++ ajax/upload.php
@@ -5,8 +5,10 @@
  * Date: 2018-08-23
  * Time: 16:32
  */
+var_dump($_POST);
 $file =  iconv('utf-8','gbk',$_FILES['userpic']['name']);
 if (move_uploaded_file($_FILES['userpic']['tmp_name'],'./upload/'.$file)) {
+    echo $file;
     echo '上传成功';
 } else {
     echo '上传失败';
--- pdoError.php
+++ pdoError.php
@@ -7,17 +7,46 @@
  */
 
 //实例化pdo对象
-$dsn = "mysql:host=106.75.129.183;dbname=test;port=3306;charset=utf8";
-$user = 'root';
-$pass = 'myroot123';
-$pdo = new PDO($dsn,$user,$pass);
+//$dsn = "mysql:host=106.75.129.183;dbname=test;port=3306;charset=utf8";
+//$user = 'root';
+//$pass = 'myroot123';
+//$pdo = new PDO($dsn,$user,$pass);
+//
+////user表不存在
+//$sql = "SELECT * FROM use";
+//$pao_statement = $pdo->prepare($sql);
+//$pao_statement->execute();
+//$result = $pao_statement->errorInfo();
+//echo "<pre>";
+//
+//echo "test";
+//var_dump($result);
+//$a = null;
+//var_dump($a);
+//if ( isset($a)) {
+//    echo "存在";
+//} else {
+//    echo "不存在";
+//}
 
-//user表不存在
-$sql = "SELECT * FROM use";
-$pao_statement = $pdo->prepare($sql);
-$pao_statement->execute();
-$result = $pao_statement->errorInfo();
-echo "<pre>";
+//$a = 1;
+//$b = 1;
+//if ( $a===$b ) {
+//    echo "全等于";
+//}  else {
+//    echo "全部等于";
+//}
 
-echo "test";
-var_dump($result);
+//$a = 10 ;
+//$c = ++$a + $a++;
+//echo $c;
+
+for ( $i=1;$i<=6;$i++) {
+    for ( $k=1;$k<=6-$i;$k++) {
+        echo "&nbsp;";
+    }
+    for ( $j=1;$j<=2*$i-1;$j++) {
+        echo "*";
+    }
+    echo "<br/>";
+}
